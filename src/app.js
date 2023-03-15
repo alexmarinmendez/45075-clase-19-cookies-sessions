799ecfc40346a93e1d2befa6940854a78b8439b6
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-15 18:47:44 -0500

    feat: cookie preference example

--- src/app.js
+++ src/app.js
@@ -5,21 +5,37 @@ const app = express()
 
 app.use(cookieParser())
 
-app.get('/', (req, res) => res.send('0k'))
-
-//set the cookie
-app.get('/cookie/set', (req, res) => {
-    res.cookie('login', 'true').send('Cookie seteada')
+app.get('/preference', (req, res) => {
+    let preference = {
+        language: 'es',
+        mode: 'dark',
+        login: true
+    }
+    res.cookie('preference', JSON.stringify(preference)).send('Configuraciones guardadas con éxito.')
 })
 
-//get the cookie
-app.get('/cookie/get', (req, res) => {
-    res.send(req.cookies)
+app.get('/control-panel', (req, res) => {
+    if (req.cookies.preference) {
+        let preference = JSON.parse(req.cookies.preference)
+        res.send(`Bienvenido de vuelta. Tus configuraciones son: MODE=${preference.mode} LANGUAGE=${preference.language}`)
+    } else {
+        res.send('No estas logueado.')
+    }
 })
 
-//delete the cookie
-app.get('/cookie/delete', (req, res) => {
-    res.clearCookie('login').send('Cookie removed!')
-})
+// //set the cookie
+// app.get('/cookie/set', (req, res) => {
+//     res.cookie('login', 'true').send('Cookie seteada')
+// })
+
+// //get the cookie
+// app.get('/cookie/get', (req, res) => {
+//     res.send(req.cookies)
+// })
+
+// //delete the cookie
+// app.get('/cookie/delete', (req, res) => {
+//     res.clearCookie('login').send('Cookie removed!')
+// })
 
 app.listen(8080, () => console.log('Server Up'))
