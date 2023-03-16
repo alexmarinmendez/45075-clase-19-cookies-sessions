5c3da12f69701100d090213b5ba9b090fb7a244e
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-15 19:54:13 -0500

    feat: actividad de clase

--- src/app.js
+++ src/app.js
@@ -1,53 +1,20 @@
 import express from 'express'
-import session from 'express-session'
+import cookieParser from 'cookie-parser'
+import __dirname from './utils.js'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(express.static(__dirname+'/public'))
 
-app.use(session({
-    secret: 'c0d3r',
-    resave: true,
-    saveUninitialized: true
-}))
-
-app.get('/preference', (req, res) => {
-    let preference = {
-        language: 'es',
-        mode: 'dark',
-        login: true
-    }
-    req.session.preference = preference
-    res.send('Configuraciones guardadas con éxito.')
+app.post('/set', (req, res) => {
+    let datos = { name: req.body.name, email: req.body.email }
+    res.cookie('actividad', JSON.stringify(datos)).redirect('/index.html')
 })
 
-app.get('/control-panel', (req, res) => {
-    if (req.session.preference) {
-        let preference = req.session.preference
-        res.send(`Bienvenido de vuelta. Tus configuraciones son: MODE=${preference.mode} LANGUAGE=${preference.language}`)
-    } else {
-        res.send('No estas logueado.')
-    }
+app.get('/get', (req, res) => {
+    res.json(JSON.parse(req.cookies.actividad))
 })
-
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) return res.send('Logout error')
-        return res.send('Loogout OK')
-    })
-})
-
-// //set the cookie
-// app.get('/cookie/set', (req, res) => {
-//     res.cookie('login', 'true').send('Cookie seteada')
-// })
-
-// //get the cookie
-// app.get('/cookie/get', (req, res) => {
-//     res.send(req.cookies)
-// })
-
-// //delete the cookie
-// app.get('/cookie/delete', (req, res) => {
-//     res.clearCookie('login').send('Cookie removed!')
-// })
 
 app.listen(8080, () => console.log('Server Up'))
